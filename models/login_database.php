8e9c3f041a67e5021fded005aa85e80f033ee8a7
loumalou <Lou.longtin@technowait.com>
2018-07-03 16:16:16 -0400

    Update login_database.php
    Fix over }

--- models/login_database.php
+++ models/login_database.php
@@ -1,59 +1,53 @@
 <?php
 
-   Class Login_Database extends CI_Model {
+Class Login_Database extends CI_Model {
 
-     public function reg_insert($data) {
-       // queries
-       $condition = "user_name =" . "'" . $data['username'] . "'";
-       $this->db->select('*');
-       $this->db->from('user_login');
-       $this->db->where($condition);
-       $this->db->limit(1);
-       $query = $this->db->get();
-       if ($query->num_rows() == 0) {
+    public function reg_insert($data) {
+        // queries
+        $condition = "user_name =" . "'" . $data['username'] . "'";
+        $this->db->select('*');
+        $this->db->from('user_login');
+        $this->db->where($condition);
+        $this->db->limit(1);
+        $query = $this->db->get();
+        if ($query->num_rows() == 0) {
+            $this->db->insert('user_login', $data);
+            if ($this->db->affected_rows() > 0) {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
 
-         $this->db->insert('user_login', $data);
-         if ($this->db->affected_rows() > 0) {
-           return true;
-         }
-       } else {
-         return false;
-       }
-     }
+    public function login($data) {
+        $condition = "user_name =" . "'" . $data['username'] . "' AND " . "user_password =" . "'" . $data['password'] . "'";
+        $this->db->select('*');
+        $this->db->from('user_login');
+        $this->db->where($condition);
+        $this->db->limit(1);
+        $query = $this->db->get();
+        if ($query->num_rows() == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-     public function login($data) {
-
-       $condition = "user_name =" . "'" . $data['username'] . "' AND " . "user_password =" . "'" . $data['password'] . "'";
-       $this->db->select('*');
-       $this->db->from('user_login');
-       $this->db->where($condition);
-       $this->db->limit(1);
-       $query = $this->db->get();
-
-       if ($query->num_rows() == 1) {
-         return true;
-       }
-     } else {
-       return false;
-     }
-   }
-
-   public function read_user_information($username) {
-
-     $condition = "user_name =" . "'" . $username . "'";
-     $this->db->select('*');
-     $this->db->from('user_login');
-     $this->db->where($condition);
-     $this->db->limit(1);
-     $query = $this->db->get();
-
-     if ($query->num_rows() == 1) {
-       return true;
-     }
-   } else {
-     return false;
-   }
- }
+    public function read_user_information($username) {
+        $condition = "user_name =" . "'" . $username . "'";
+        $this->db->select('*');
+        $this->db->from('user_login');
+        $this->db->where($condition);
+        $this->db->limit(1);
+        $query = $this->db->get();
+        if ($query->num_rows() == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
+
 ?>
